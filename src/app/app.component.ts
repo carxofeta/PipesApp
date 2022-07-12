4eb113204d4d88700f171b4755da0c5d7e763857
carxofeta <javimarco@gmail.com>
2022-07-12 19:39:56 +0200

    Preliminar version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+
+  nombre: string = 'jAvier mArcO';
+  valor: number = 1000;
+  obj = {
+    nombre: "Fernanda"
+  }
+
+  mostrarNombre() {
+    console.log( this.nombre )
+    console.log( this.valor )
+    console.log( this.obj )
+  }
 }
